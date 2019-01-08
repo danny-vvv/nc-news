7f20b564e14cbf17b4b2c8f1b0b1103044d39719
Daniel Varcas <daniel.varcas@gmail.com>
2019-01-08 11:21:42 +0000

    add conditional request for all articles if on home page

--- src/api.js
+++ src/api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:9090/api';
 
 export const fetchArticles = async (topic) => {
-  const { data } = await axios.get(`${BASE_URL}/topics/${topic}/articles`);
-  console.log(data)
+  const { data } = topic
+    ? await axios.get(`${BASE_URL}/topics/${topic}/articles`)
+    : await axios.get(`${BASE_URL}/articles`);
   return data;
 }
