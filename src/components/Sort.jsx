c2103ec5fd0a0e80625c3963b05e1446e15b6a4c
Daniel Varcas <daniel.varcas@gmail.com>
2019-01-19 22:44:24 +0000

    fix Sort lint issues

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,60 +1,71 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles, Select, Input, FormControl, MenuItem } from '@material-ui/core';
-
-const styles = theme => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    formControl: {
-        margin: theme.spacing.unit,
-        minWidth: 120,
-    },
-    selectEmpty: {
-        marginTop: theme.spacing.unit * 2,
-    },
-});
+import {
+  withStyles, Select, Input, FormControl, MenuItem,
+} from '@material-ui/core';
 
 class Sort extends Component {
     state = {
-        sort_by: 'comment_count'
+      sort_by: 'comment_count',
     }
-    render() {
-        const { options, classes } = this.props;
 
-        return (
-            <form className={classes.root} autoComplete='off'>
-                <FormControl className={classes.formControl}>
-                    <Select
-                        value={this.state.sort_by}
-                        onChange={(event) => this.handleChange(event)}
-                        input={<Input name='sort_by' id='sort-label-placeholder' />}
-                        name='sort_by'
-                        className={classes.selectEmpty}
-                    >
-                        {options.map(option => (
-                            <MenuItem key={option.value} value={option.value}>{option.name}</MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-            </form>
-        );
-    }
     handleChange = (event) => {
-        const sort_by = event.target.value;
-        this.changeSortBy(sort_by)
-        this.setState({ sort_by })
+      const sort_by = event.target.value;
+      this.changeSortBy(sort_by);
+      this.setState({ sort_by });
     }
 
     changeSortBy(sort_by) {
-        this.props.updateParentState({ sort_by })
+      const { updateParentState } = this.props;
+      updateParentState({ sort_by });
     }
-};
+
+    render() {
+      const { sort_by } = this.state;
+      const { classes, options } = this.props;
+
+      return (
+        <form className={classes.root} autoComplete="off">
+          <FormControl className={classes.formControl}>
+            <Select
+              value={sort_by}
+              onChange={event => this.handleChange(event)}
+              input={<Input name="sort_by" id="sort-label-placeholder" />}
+              name="sort_by"
+              className={classes.selectEmpty}
+            >
+              {options.map(option => (
+                <MenuItem key={option.value} value={option.value}>{option.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </form>
+      );
+    }
+}
 
 Sort.propTypes = {
-    updateParentState: PropTypes.func.isRequired,
-    options: PropTypes.arrayOf(PropTypes.object).isRequired
+  updateParentState: PropTypes.func.isRequired,
+  options: PropTypes.arrayOf(PropTypes.object).isRequired,
+  classes: PropTypes.shape({
+    root: PropTypes.string,
+    formControl: PropTypes.string,
+    selectEmpty: PropTypes.string,
+  }).isRequired,
 };
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing.unit * 2,
+  },
+});
 
 export default withStyles(styles)(Sort);
