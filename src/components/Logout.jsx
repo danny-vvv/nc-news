40fcd7a1f57e32a8cc1d71f773fd52abe6351a16
Daniel Varcas <daniel.varcas@gmail.com>
2019-01-19 19:50:55 +0000

    fix Login lint errors

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { Link } from '@reach/router';
 import { Button } from '@material-ui/core';
+import PropTypes from 'prop-types';
 
-const Logout = props => {
+const Logout = (props) => {
   const { username, changeLoginState } = props;
 
-  function handleClick() {
-    const username = '';
-    const userId = '';
-    changeLoginState({ username, userId })
-  }
-
   return (
-    <div className='Logout'>
-      <p>Welcome, {username}</p>
-      <Button onClick={handleClick}>Logout</Button>
+    <div className="Logout">
+      <p>
+Welcome,
+        {' '}
+        {username}
+      </p>
+      <Button onClick={() => changeLoginState({ username: '', userId: '' })}>Logout</Button>
       <br />
       <Button component={Link} to={`/users/${username}`}>Profile</Button>
     </div>
   );
+};
+
+Logout.propTypes = {
+  username: PropTypes.string.isRequired,
+  changeLoginState: PropTypes.func.isRequired,
 };
 
 export default Logout;
