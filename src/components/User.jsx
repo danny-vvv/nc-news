7d8a356b72e81f110f800c6bda994a3bfb0cf210
Daniel Varcas <daniel.varcas@gmail.com>
2019-01-19 22:47:41 +0000

    fix User lint issues

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,32 +1,51 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import * as api from '../api';
 
 class User extends Component {
   state = {
-    user: {}
-  }
-  render() {
-    const { user } = this.state;
-    const { username, avatar_url, name } = user;
-    return (
-      <div className='User'>
-        <img src={avatar_url} alt={`${username}'s avatar`}></img>
-        <p>Username: {username}</p>
-        <p>Real name: {name}</p>
-      </div>
-    );
+    user: {},
   }
 
   componentDidMount() {
-    const { username } = this.props;
-    this.fetchUser(username)
-    this.props.setHeading(`User: ${username}`)
+    const { username, setHeading } = this.props;
+    this.fetchUser(username);
+    setHeading(`User: ${username}`);
   }
 
   fetchUser(username) {
     api.fetchUser(username)
-      .then(({ user }) => this.setState({ user: user }))
+      .then(({ user }) => this.setState({ user }));
+  }
+
+  render() {
+    const { user } = this.state;
+    const { username, avatar_url, name } = user;
+    return (
+      <div className="User">
+        <img src={avatar_url} alt={`${username}'s avatar`} />
+        <p>
+Username:
+          {' '}
+          {username}
+        </p>
+        <p>
+Real name:
+          {' '}
+          {name}
+        </p>
+      </div>
+    );
   }
 }
+
+User.propTypes = {
+  username: PropTypes.string,
+  setHeading: PropTypes.func.isRequired,
+};
+
+User.defaultProps = {
+  username: '',
+};
 
 export default User;
