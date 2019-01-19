b26c4f5019832c7b2af28a24426f24d701f787dd
Daniel Varcas <daniel.varcas@gmail.com>
2019-01-19 19:38:21 +0000

    fix Header lint errors and add utils

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import capitalise from '../utils/capitalise';
 
 const Header = (props) => {
-
-  const heading = props.heading
-    ? props.heading[0].toUpperCase() + props.heading.slice(1)
-    : 'Home';
+  const { heading } = props;
 
   return (
-    <div className='Header' >
+    <div className="Header">
       <header>
-        <Typography variant='h2' color='inherit'>
-          {heading}
+        <Typography variant="h2" color="inherit">
+          {heading && capitalise(heading)}
         </Typography>
       </header>
     </div>
   );
+};
 
+Header.propTypes = {
+  heading: PropTypes.string,
+};
+
+Header.defaultProps = {
+  heading: 'Home',
 };
 
 export default Header;
---
+++ src/utils/capitalise.js
@@ -0,0 +1,3 @@
+const capitalise = word => word[0].toUpperCase() + word.slice(1);
+
+export default capitalise;
