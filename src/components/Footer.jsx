d46347ed627410fb921bb70432d9e3e381942309
Daniel Varcas <daniel.varcas@gmail.com>
2019-01-19 18:59:10 +0000

    fix Footer lint errors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 
-const Footer = () => {
-  return (
-    <div className="Footer">
-      <Button href='https://github.com/danielvarcas/nc-news' color='primary'>GitHub</Button>
-    </div>
-  );
-};
+const Footer = () => (
+  <div className="Footer">
+    <Button href="https://github.com/danielvarcas/nc-news" color="primary">GitHub</Button>
+  </div>
+);
 
 export default Footer;
